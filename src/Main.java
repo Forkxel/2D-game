c4d0c4cd46d232d3731c9f861d87f9b3977b5df6
Forkxel <pavel.halik06@gmail.com>
2024-04-27 21:16:14 +0200

    Created JFrame for game

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+}
